620f6032886e4ce011423639383ea7f36eb7919a
ArvinH <arvin0731@gmail.com>
2013-05-16 09:27:54 +0800

    first get data into data.csv and data.xlsx

--- DataParser/parser.py
+++ DataParser/parser.py
@@ -11,16 +11,23 @@ class Parser:
         print(req_url)
         response = urllib.request.urlopen(req_url)
         str_response = response.read().decode('utf-8')
-        print(str_response)
         obj = json.loads(str_response)
-        print(json.dumps(obj,sort_keys=True, indent=4, separators=(',', ': ')))
-        
-
-
+        #print(json.dumps(obj,sort_keys=True, indent=4, separators=(',', ': ')))
+        file = open('data_csv/data.csv','w', newline='')
+        csvW = csv.writer(file)
+        if obj['query']['results'] is not None:
+            for i in obj['query']['results']['photo']:
+                photo_id = i['id']
+                latitude = i['location']['latitude']
+                longitude = i['location']['longitude']
+                csvW.writerow([photo_id,latitude,longitude])
+                #csvW.writerow('\n')
+        else:
+            print('result null')
 def main():
     queryText = '孔廟'
-    p = Parser('http://query.yahooapis.com/v1/public/yql?q=select%20id%2Ctitle%2Clocation.latitude%2Clocation.longitude%2Cdates.taken%2Cfarm%2Cserver%2Csecret%20from%20flickr.photos.info%20where%20photo_id%20in%20(select%20id%20from%20flickr.photos.search(0)%20where%20text%3D%22%E5%AD%94%E5%BB%9F%22%20and%20has_geo%3D1%20and%20lat%3D22.993299484253%20and%20lon%3D120.20359802246%20and%20content_type%3D1%20and%20radius%3D20%20and%20api_key%3D%2263d0f7b2e9592d8f5ad413cc5c60e551%22%20limit%20100%20)%20and%20api_key%3D%2263d0f7b2e9592d8f5ad413cc5c60e551%22&format=json&callback=','flickr_data')
-    #p = Parser('http://query.yahooapis.com/v1/public/yql?q=select id,title,location.latitude,location.longitude,dates.taken,farm,server,secret from flickr.photos.info where photo_id in ( select id from flickr.photos.search(0) where text="'+queryText+'" and has_geo=1 and lat=22.993299484253 and lon=120.20359802246 and content_type=1 and radius=20 and api_key="63d0f7b2e9592d8f5ad413cc5c60e551" limit 100 ) and api_key="63d0f7b2e9592d8f5ad413cc5c60e551" and format=json','opengov_data')
+    #p = Parser('http://query.yahooapis.com/v1/public/yql?q=SELECT%20*%20FROM%20flickr.people.info2%20WHERE%20user_id%3D%2251035801340%40N01%22%20and%20api_key%3D%2263d0f7b2e9592d8f5ad413cc5c60e551%22&format=json&callback=','flickr_data')
+    p = Parser('http://query.yahooapis.com/v1/public/yql?q=select%20id%2Ctitle%2Clocation.latitude%2Clocation.longitude%2Cdates.taken%2Cfarm%2Cserver%2Csecret%20from%20flickr.photos.info%20where%20photo_id%20in%20(select%20id%20from%20flickr.photos.search(0)%20where%20text%3D%22%E5%AD%94%E5%BB%9F%22%20and%20has_geo%3D1%20and%20lat%3D22.993299484253%20and%20lon%3D120.20359802246%20and%20content_type%3D1%20and%20radius%3D20%20and%20api_key%3D%2263d0f7b2e9592d8f5ad413cc5c60e551%22%20limit%20100%20)%20and%20api_key%3D%2263d0f7b2e9592d8f5ad413cc5c60e551%22&format=json&callback=','opengov_data')
     ###p = Parser('http://query.yahooapis.com/v1/public/yql?q=select%20id%2Ctitle%2Clocation.latitude%2Clocation.longitude%2Cdates.taken%2Cfarm%2Cserver%2Csecret%20from%20flickr.photos.info%20where%20photo_id%20in%20(select%20id%20from%20flickr.photos.search(0)%20where%20text%3D%22'+queryText+'%22%20and%20has_geo%3D1%20and%20lat%3D22.993299484253%20and%20lon%3D120.20359802246%20and%20content_type%3D1%20and%20radius%3D20%20and%20api_key%3D%2263d0f7b2e9592d8f5ad413cc5c60e551%22%20limit%20100%20)%20and%20api_key%3D%2263d0f7b2e9592d8f5ad413cc5c60e551%22&format=json','opengov_data')
     p.parse()
 
